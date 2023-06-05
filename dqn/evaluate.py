cbaed4edea262a5e3f76284a011586ff860f6fe7
Adam Bodnar <adam.bodnar10@gmail.com>
2023-06-05 18:46:59 +0200

    Split train and evaluation logic
    * Functions regarding evaluation were moved to separate file

--- dqn/evaluate.py
+++ dqn/evaluate.py
@@ -1,1 +1,50 @@
-# TODO
+import numpy as np
+
+
+def create_opponent_pairs(agents, random_bots, num_players, num_configs):
+    opponent_pairs = []
+    # Each agent against random bot
+    for ag in agents:
+        if ag.player_id == 0:
+            opponent_pairs.append((ag, random_bots[1]))
+        else:
+            opponent_pairs.append((random_bots[0], ag))
+
+    # Same config agents against each other
+    for i in range(0, len(agents), num_players):
+        opponent_pairs.append((agents[i], agents[i + 1]))
+
+    # Different config agents against each other
+    for i in range(num_configs - 1):
+        for j in range(i + 1, num_configs):
+            opponent_pairs.append((agents[i * num_players], agents[j * num_players + 1]))
+            opponent_pairs.append((agents[j * num_players], agents[i * num_players + 1]))
+
+    return opponent_pairs
+
+
+def eval_agents(env, opponent_pairs, num_episodes):
+    num_players = len(opponent_pairs[0])
+    sum_episode_rewards = np.zeros((len(opponent_pairs), num_players))
+    # Evaluate each agent pair
+    for pair_idx, opponents in enumerate(opponent_pairs):
+        for _ in range(num_episodes):
+            # Simulate full game
+            time_step = env.reset()
+            episode_rewards = np.zeros(num_players)
+            while not time_step.last():
+                player_id = time_step.observations["current_player"]
+                if env.is_turn_based:
+                    agent_output = opponents[player_id].step(
+                        time_step, is_evaluation=True)
+                    action_list = [agent_output.action]
+                else:
+                    agents_output = [agent.step(time_step, is_evaluation=True) for agent in opponents]
+                    action_list = [agent_output.action for agent_output in agents_output]
+
+                time_step = env.step(action_list)
+                episode_rewards += time_step.rewards
+            # Accumulate rewards
+            sum_episode_rewards[pair_idx] += episode_rewards
+
+    return sum_episode_rewards / num_episodes
--- dqn/train.py
+++ dqn/train.py
@@ -2,58 +2,12 @@ from open_spiel.python.algorithms import dqn
 from open_spiel.python import rl_environment
 from open_spiel.python.algorithms import random_agent
 
-from games.tic_tac_toe import register_pyspiel
+from games.tic_tac_toe import TTT_NAME
+from games.snakes import SNAKES_NAME
 
-import numpy as np
 import tensorflow.compat.v1 as tf
 
-
-def eval_agents(env, opponent_pairs, num_episodes):
-    num_players = len(opponent_pairs[0])
-    sum_episode_rewards = np.zeros((len(opponent_pairs), num_players))
-    for pair_idx, opponents in enumerate(opponent_pairs):
-        for _ in range(num_episodes):
-            time_step = env.reset()
-            episode_rewards = np.zeros(num_players)
-            while not time_step.last():
-                player_id = time_step.observations["current_player"]
-                if env.is_turn_based:
-                    agent_output = opponents[player_id].step(
-                        time_step, is_evaluation=True)
-                    action_list = [agent_output.action]
-                else:
-                    agents_output = [
-                        agent.step(time_step, is_evaluation=True) for agent in opponents
-                    ]
-                    action_list = [agent_output.action for agent_output in agents_output]
-
-                time_step = env.step(action_list)
-                episode_rewards += time_step.rewards
-            sum_episode_rewards[pair_idx] += episode_rewards
-
-    return sum_episode_rewards / num_episodes
-
-
-def create_opponent_pairs(agents, random_bots, num_players, num_configs):
-    opponent_pairs = []
-    # Each agent against random bot
-    for ag in agents:
-        if ag.player_id == 0:
-            opponent_pairs.append((ag, random_bots[1]))
-        else:
-            opponent_pairs.append((random_bots[0], ag))
-
-    # Same config agents against each other
-    for i in range(0, len(agents), num_players):
-        opponent_pairs.append((agents[i], agents[i + 1]))
-
-    # Different config agents against each other
-    for i in range(num_configs - 1):
-        for j in range(i + 1, num_configs):
-            opponent_pairs.append((agents[i * num_players], agents[j * num_players + 1]))
-            opponent_pairs.append((agents[j * num_players], agents[i * num_players + 1]))
-
-    return opponent_pairs
+from evaluate import eval_agents, create_opponent_pairs
 
 
 def train_eval(game_config, agents_config):
@@ -84,20 +38,23 @@ def train_eval(game_config, agents_config):
 
         num_configs = len(agents) // num_players
         opponent_pairs = create_opponent_pairs(agents, random_bots, num_players, num_configs)
-        evaluation = []
 
+        rewards = []
+        losses = []
         for ep in range(game_config['num_train_episodes']):
             # Evaluate gradually
             if (ep + 1) % game_config['eval_every'] == 0:
                 print(f"Evaluating after {ep + 1} episodes.")
                 mean_rewards = eval_agents(env, opponent_pairs, game_config['num_eval_episodes'])
-                evaluation.append(mean_rewards)
+                rewards.append(mean_rewards)
 
             # Train DQN agents for each config
             for cfg_idx in range(1, num_configs + 1):
+                # Grab agents for current config
                 begin, end = (cfg_idx - 1) * num_players, (cfg_idx - 1) * num_players + 1
-                curr_agents = agents[begin:end+1]
+                curr_agents = agents[begin:end + 1]
 
+                # Simulate a full game
                 time_step = env.reset()
                 while not time_step.last():
                     player_id = time_step.observations["current_player"]
@@ -109,25 +66,17 @@ def train_eval(game_config, agents_config):
                         action_list = [agent_output.action for agent_output in agents_output]
                     time_step = env.step(action_list)
 
-                # Episode is over, step all agents with final info state.
+                # Step all agents with final info state
                 for agent in curr_agents:
                     agent.step(time_step)
-        return evaluation
+
+        return losses, rewards
 
 
 if __name__ == "__main__":
-    register_pyspiel(5, 5, 3, "ttt")
-
-    game_cfg = {
-        'game': 'ttt',
-        'num_players': 2,
-        'num_train_episodes': 10_000,
-        'eval_every': 1000,
-        'num_eval_episodes': 250
-    }
-
-    TTT_cfg_small = {
-        'name': 'TTT_small',
+    # Train three DQN with increasing size
+    DQN_small = {
+        'name': 'DQN_small',
         'batch_size': 32,
         'learn_every': 2,
         'update_target_network_every': 100,
@@ -135,13 +84,42 @@ if __name__ == "__main__":
         'hidden_layers_sizes': [32, 32]
     }
 
-    TTT_cfg_big = {
-        'name': 'TTT_big',
-        'batch_size': 32,
+    DQN_medium = {
+        'name': 'DQN_medium',
+        'batch_size': 64,
         'learn_every': 2,
         'update_target_network_every': 100,
         'optimizer': 'adam',
-        'hidden_layers_sizes': [64, 128, 64]
+        'hidden_layers_sizes': [256, 128, 256]
     }
 
-    rewards = train_eval(game_cfg, [TTT_cfg_small, TTT_cfg_big])
+    DQN_large = {
+        'name': 'DQN_large',
+        'batch_size': 128,
+        'learn_every': 2,
+        'update_target_network_every': 100,
+        'optimizer': 'adam',
+        'hidden_layers_sizes': [1024, 512, 512, 512, 1024]
+    }
+
+    DQN_configs = [DQN_small, DQN_medium, DQN_large]
+
+    # Train on TTT, Snakes and Nim
+    game_cfg = {
+        'game': TTT_NAME,
+        'num_players': 2,
+        'num_train_episodes': 10_000,
+        'eval_every': 1000,
+        'num_eval_episodes': 250
+    }
+
+    # TTT
+    TTT_losses, TTT_gradual_rewards = train_eval(game_cfg, DQN_configs)
+
+    # # Snakes
+    # game_cfg['game'] = SNAKES_NAME
+    # Snakes_losses, Snakes_gradual_rewards = train_eval(game_cfg, DQN_configs)
+    #
+    # # Nim
+    # game_cfg['game'] = 'nim'
+    # Nim_losses, Nim_gradual_rewards = train_eval(game_cfg, DQN_configs)
